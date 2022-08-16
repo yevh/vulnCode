7658313ddca9fe2b94119ecab317d1ab4394b32d
Yevhenii Molchanov <oocrazyb@gmail.com>
2022-08-16 15:11:29 +0300

    Update datacreator.ts

--- datacreator.ts
+++ datacreator.ts
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
+ * SPDX-License-Identifier: MITdsadsa
  */
 
 /* jslint node: true */
