d15021720297616a142cb81b5823f61e9f303b03
Yevhenii Molchanov <oocrazyb@gmail.com>
2023-03-28 10:33:16 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.dsadsa
  * SPDX-License-Identifier: MIT
  */
 import dataErasure from './routes/dataErasure'
